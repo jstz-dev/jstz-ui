67475a013f146c76b38ee6333c22b645bc5a30eb
hubcio2115 <kowalski.hubercik1@gmail.com>
2025-10-06 12:55:34 +0200

    bumped package versions to the latest minors

--- eslint.config.js
+++ eslint.config.js
@@ -3,23 +3,31 @@ import js from "@eslint/js";
 
 import reactHooks from "eslint-plugin-react-hooks";
 import storybook from "eslint-plugin-storybook";
-import { globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(
   [
     globalIgnores(["dist"]),
     {
       files: ["**/*.{ts,tsx}"],
       extends: [
         js.configs.recommended,
-        tseslint.configs.recommended,
+        ...tseslint.configs.strictTypeChecked,
         reactHooks.configs["recommended-latest"],
       ],
       languageOptions: {
         ecmaVersion: 2020,
         globals: globals.browser,
+        parserOptions: {
+          tsconfigRootDir: __dirname,
+        },
       },
     },
   ],
